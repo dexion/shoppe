d2c4c071dfa37a3229158f47c72e8f86e66f53c4
Tristaan <tristan.sneider@gmail.com>
2017-03-14 02:56:52 +0100

    fixed bug

--- app/uploaders/shoppe/attachment_uploader.rb
+++ app/uploaders/shoppe/attachment_uploader.rb
@@ -21,21 +21,6 @@ class Shoppe::AttachmentUploader < CarrierWave::Uploader::Base
   # Create different versions of your uploaded files:
   version :thumb, if: :image? do
     process mogrify: [500, 500]
-  end
-  def mogrify(options = {})
-      manipulate! do |img|
-          img.format("png") do |c|
-              c.fuzz        "0%"
-              c.trim
-              c.rotate      "#{options[:rotate]}" if options.has_key?(:rotate)
-              c.resize      "#{options[:resolution]}>" if options.has_key?(:resolution)
-              c.resize      "#{options[:resolution]}<" if options.has_key?(:resolution)
-              c.push        '+profile'
-              c.+           "!xmp,*"
-              c.profile     "#{Rails.root}/lib/color_profiles/sRGB_v4_ICC_preference_displayclass.icc"
-              c.colorspace  "sRGB"
-          end
-          img
-      end
+    process quality: 100
   end
 end
